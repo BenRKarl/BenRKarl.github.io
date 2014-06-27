0ed0e1b652708d6fb441015248edb06474edcd13
Ben Karl <benrkarl@gmail.com>
2014-06-27 13:53:37 -0400

    fix some of the animations and clean up the home page

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,3 +1,10 @@
+$(function(){
+  $('.main-image').css({'opacity':0}).animate({'opacity':1}, 1000);
+  $('.main-title').css({'opacity':0}).animate({'opacity':1}, 3000);
+  $('.navblock').css({'opacity':0}).animate({'opacity':1}, 5000);
+})
+
+
 
 
 // function fadeBackgroundColor(){
@@ -9,28 +16,28 @@
 //   $('body').css('backgroundColor', 'rgba(200, 100, 255, '+  scrollRatio +')');
 // }
 
-function moveMainImageLeft(){
-  var scrollTop = $(window).scrollTop();
-  var mainImg = $('.main-image');
-  if ( scrollTop < 585 ) {
-    $(mainImg).css('right', scrollTop);
-    }
-  $(mainImg).css('top', scrollTop)
-  };
+// function moveMainImageLeft(){
+//   var scrollTop = $(window).scrollTop();
+//   var mainImg = $('.main-image');
+//   if ( scrollTop < 585 ) {
+//     $(mainImg).css('right', scrollTop);
+//     }
+//   $(mainImg).css('top', scrollTop)
+//   };
 
-function rotateMainImage(){
-  var scrollTop = $(window).scrollTop();
-  var mainImg = $('.main-image');
-  if (scrollTop < 360 ) {
-    $(mainImg).css('transform', 'rotate( -' + scrollTop + 'deg)' )
-    }
-};
+// function rotateMainImage(){
+//   var scrollTop = $(window).scrollTop();
+//   var mainImg = $('.main-image');
+//   if (scrollTop < 360 ) {
+//     $(mainImg).css('transform', 'rotate( -' + scrollTop + 'deg)' )
+//     }
+// };
 
 
 
-$(window).on('scroll', function(){
+// $(window).on('scroll', function(){
 
-  moveMainImageLeft();
-  rotateMainImage();
+//   moveMainImageLeft();
+//   rotateMainImage();
 
-});
+// });
