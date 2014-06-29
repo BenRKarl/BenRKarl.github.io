76fb111c521e316be23e9b54aac327d5164c6152
Ben Karl <benrkarl@gmail.com>
2014-06-28 20:23:58 -0400

    make main image draggable

--- javascripts/script.js
+++ javascripts/script.js
@@ -24,48 +24,10 @@ $(function(){
     $('.portfolio').toggle(750);
   });
 
-
-
-  // $('.main-image').click(function(){
-  //   $(this).effect('bounce', {distance: 10, times: 1}, 300);
-  // });
+  $('.main-image').draggable();
 
 })
 
 
 
 
-// function fadeBackgroundColor(){
-//   var scrollTop = $(window).scrollTop();
-//   console.log('scrollTop: ', scrollTop);
-//   var documentHeight = $(document).height();
-//   console.log('documentHeight: ', documentHeight);
-//   var scrollRatio = scrollTop/documentHeight;
-//   $('body').css('backgroundColor', 'rgba(200, 100, 255, '+  scrollRatio +')');
-// }
-
-// function moveMainImageLeft(){
-//   var scrollTop = $(window).scrollTop();
-//   var mainImg = $('.main-image');
-//   if ( scrollTop < 585 ) {
-//     $(mainImg).css('right', scrollTop);
-//     }
-//   $(mainImg).css('top', scrollTop)
-//   };
-
-// function rotateMainImage(){
-//   var scrollTop = $(window).scrollTop();
-//   var mainImg = $('.main-image');
-//   if (scrollTop < 360 ) {
-//     $(mainImg).css('transform', 'rotate( -' + scrollTop + 'deg)' )
-//     }
-// };
-
-
-
-// $(window).on('scroll', function(){
-
-//   moveMainImageLeft();
-//   rotateMainImage();
-
-// });
